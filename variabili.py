14f6c8fa30eb5cb02474a0245ddba2b0a87a979a
Massimo <pellimassimo96@gmail.com>
2026-05-03 18:29:17 +0200

    esercizio variabili

---
+++ variabili.py
@@ -0,0 +1,12 @@
+#Crea una variabile con il tuo nome e stampala.
+nome = "Massimo"
+print(nome)
+
+#Chiedi all’utente la sua età e stampala raddoppiata.
+eta = int(input("Quanti anni hai? "))
+print("Il doppio della tua età è", eta * 2)
+
+#Crea due numeri e stampa la loro somma.
+x = 10
+y = 68
+print(x + y)

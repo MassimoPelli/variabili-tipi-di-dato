575c105e5953da6e03aad27b6781ae9ef262dd32
Massimo <pellimassimo96@gmail.com>
2026-05-03 18:38:42 +0200

    esercizio tipi di dati

---
+++ tipi_di_dati.py
@@ -0,0 +1,19 @@
+#Crea una variabile x con valore 7 e una y con valore 3. Stampa la somma dei due numeri.
+x = 7
+y = 3
+print(x + y)
+
+#Crea una variabile prezzo con valore 19.99 e una quantità con valore 3. Stampa il totale (prezzo × quantità).
+prezzo = 19.99
+quantità = 3
+print("Totale =", + prezzo * quantità)
+
+#Crea una variabile numero con valore 10. Stampa True se il numero è maggiore di 5, altrimenti False.
+numero = 10
+print(numero > 5)
+
+#Prova a combinare un int e un float: assegna a a = 5 e b = 2.5, poi calcola la loro somma.
+a = 5
+b = 2.5
+somma = 5 + 2.5
+print(somma)
